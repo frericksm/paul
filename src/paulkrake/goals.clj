c9e18d894496f49e6e3d9360c6b5c2b49e0e2f70
Michael Frericks <Michael.Frericks@f-i.de>
2014-10-28 10:22:55 +0100

    refactor

--- src/paulkrake/goals.clj
+++ src/paulkrake/goals.clj
@@ -1,22 +1,21 @@
 (ns paulkrake.goals
   (:require [paulkrake.score :as s]
-            [paulkrake.predict :as p]))
+            [paulkrake.predict :as p]
+            [paulkrake.spielplan :as sp]
+            [incanter.distributions :as d]))
 
 (defn goals-to-score-fn
-  "Maps number-of-goals to a score value from [0 1]"
-  [number-of-goals]
-  (let [base 0.46   ;; Base zur Berechnung des Scores aus den Goals
-        goals-as-number (if (number? number-of-goals)
-                          number-of-goals
-                          (Integer/valueOf number-of-goals))]
-    (- 1.0 (Math/pow base goals-as-number))))
+  "Maps number-of-goals to a score value from [0 1]. The mean of goals per game is 1.36"
+  [goals]
+  (let [goals-as-number (if (number? goals) goals (Integer/valueOf goals))]
+    (d/cdf (d/poisson-distribution 1.36) goals-as-number)))
 
 (defn score-to-goals-fn [score]
-  (as-> (range) x
+  (as-> (range -1 100) x
         (partition 2 1 x)
         (filter (fn [[a b]]
                   (and (<= (goals-to-score-fn a) score )
-                       (< score (goals-to-score-fn b)))) x)
+                       (<= score (goals-to-score-fn b)))) x)
         (map (fn [[a b]]
                (+ a (*  (/ (- score (goals-to-score-fn a)) 
                            (- (goals-to-score-fn b) (goals-to-score-fn a))) 
@@ -33,3 +32,10 @@
      (as-> games x
            (map (fn [[ h g]] (p/predict-single-game data h g faktor-sigma score-to-goals-fn)) x)
            (map (fn [[h g [hmin hmax] [gmin gmax]]] (format "%24s - %24s   [%.2f - %.2f] : [%.2f - %.2f]" h g hmin hmax gmin gmax)) x))))
+
+
+(defn goal-data [saison spieltag-nr]
+  (as-> (s/initial-rating-data (s/vereine (sp/spieltag 1415 1))) x
+        (reduce (fn [a i] (new-rating-goals a
+                                           (sp/spieltag 1415 i)))
+                x (range 1 spieltag-nr))))
--- src/paulkrake/shots.clj
+++ src/paulkrake/shots.clj
@@ -1,18 +1,21 @@
 (ns paulkrake.shots
   (:require [paulkrake.score :as s]
-            [paulkrake.predict :as p]))
+            [paulkrake.predict :as p]
+            [paulkrake.spielplan :as sp]
+            [incanter.distributions :as d]))
 
 (defn shots-to-score-fn
-  "Maps number-of-goals to a score value from [0 1]"
-  [number-of-goals]
-  (/ (min 24 number-of-goals) 25.0))
+  "Maps number of shots to a score value from [0 1].
+   The mean of shots per game is 12.81."
+  [shots]
+  (d/cdf (d/poisson-distribution 12.81) shots))
 
 (defn score-to-shots-fn [score]
-  (as-> (range) x
+  (as-> (range -1 100) x
         (partition 2 1 x)
         (filter (fn [[a b]]
                   (and (<= (shots-to-score-fn a) score )
-                       (< score (shots-to-score-fn b)))) x)
+                       (<= score (shots-to-score-fn b)))) x)
         (map (fn [[a b]]
                (+ a (*  (/ (- score (shots-to-score-fn a)) 
                            (- (shots-to-score-fn b) (shots-to-score-fn a))) 
@@ -29,3 +32,9 @@
      (as-> games x
            (map (fn [[ h g]] (p/predict-single-game data h g faktor-sigma score-to-shots-fn)) x)
            (map (fn [[h g [hmin hmax] [gmin gmax]]] (format "%24s - %24s   [%.2f - %.2f] : [%.2f - %.2f]" h g hmin hmax gmin gmax)) x))))
+
+(defn shots-data [saison spieltag-nr]
+  (as-> (s/initial-rating-data (s/vereine (sp/spieltag 1415 1))) x
+        (reduce (fn [a i] (new-rating-shots a
+                                           (sp/spieltag-shots 1415 i)))
+                x (range 1 spieltag-nr))))
